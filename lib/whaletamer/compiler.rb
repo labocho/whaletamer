224828cbce44742680d1c0c3d80ef67c765f7e0e
labocho <labocho@penguinlab.jp>
2017-09-06 15:54:13 +0900

    support template

--- lib/whaletamer/compiler.rb
+++ lib/whaletamer/compiler.rb
@@ -1,6 +1,7 @@
 require "yaml"
 require "fileutils"
 require "pathname"
+require "erb"
 
 module Whaletamer
   class Compiler
@@ -19,6 +20,7 @@ module Whaletamer
 
       File.write(File.join(dest_dir, "Dockerfile"), compile_dockerfile(image_name))
       compile_files(image_root, dest_dir)
+      compile_templates(image_root, dest_dir, config.fetch(image_name)["attributes"] || {})
     end
 
     def compile_dockerfile(image_name)
@@ -38,6 +40,7 @@ module Whaletamer
         relative_path = Pathname.new(src).relative_path_from(Pathname.new(files_root))
         dest = File.join(dest_dir, relative_path)
         FileUtils.mkdir_p(File.dirname(dest))
+
         if src =~ /\.encrypted$/
           dest.gsub!(/\.encrypted$/, "")
           File.write(dest, decrypt(File.read(src)))
@@ -45,6 +48,25 @@ module Whaletamer
           FileUtils.cp(src, dest)
         end
       end
+    end
+
+    def compile_templates(image_root, dest_dir, attributes)
+      templates_root = File.join(image_root, "templates")
+
+      Dir.glob("#{templates_root}/**/*", File::FNM_DOTMATCH).each do |src|
+        next unless File.file?(src)
+        relative_path = Pathname.new(src).relative_path_from(Pathname.new(templates_root))
+        dest = File.join(dest_dir, relative_path)
+        FileUtils.mkdir_p(File.dirname(dest))
+
+        File.write(dest, compile_template(src, attributes))
+      end
+    end
+
+    def compile_template(src, attributes)
+      b = binding
+      b.local_variable_set(:attributes, attributes)
+      ERB.new(File.read(src), nil, "-").result(b)
     end
   end
 end
