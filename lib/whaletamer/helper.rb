88c2dc121089104aee4669731147a8c00554511c
labocho <labocho@penguinlab.jp>
2017-09-07 15:33:53 +0900

    add heper methods

--- lib/whaletamer/helper.rb
+++ lib/whaletamer/helper.rb
@@ -1,14 +1,31 @@
 require "shellwords"
 module Whaletamer
   module Helper
-    def user(name, **options)
+    def user(name, shell: nil)
       command = ["useradd", "-m"]
-      if options[:shell]
-        command += ["-s", options[:shell]]
-      end
+      command += ["-s", shell] if shell
       command << name
 
       run command.shelljoin
+    end
+
+    def directory(name, owner: nil, group: nil, mode: nil)
+      commands = [["mkdir", name]]
+      commands << ["chown", "#{owner}:#{group}", name] if owner || group
+      commands << ["chmod", mode, name] if mode
+
+      run commands.map(&:shelljoin).join(" && ")
+    end
+
+    def file(dest, owner: nil, group: nil, mode: nil)
+      src = dest.gsub(/^\//, "")
+      copy [src, dest].shelljoin
+
+      commands = []
+      commands << ["chown", "#{owner}:#{group}", dest] if owner || group
+      commands << ["chmod", mode, dest] if mode
+
+      run commands.map(&:shelljoin).join(" && ") if commands.any?
     end
   end
 end
